084acc059e2d2f66a2041bc1419a829ad99792c8
flasd <flasd2@gmail.com>
2019-01-20 17:04:28 -0200

    feat: make import to cause only sideEffects

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import { userTabbing } from './styles.scss';
+import { userTabbing } from "./styles.scss";
 
-export default function fixFocus() {
-  function handleMouseDownOnce() {
-    document.body.classList.remove(userTabbing);
+function handleMouseDownOnce() {
+  document.body.classList.remove(userTabbing);
 
-    window.removeEventListener('mousedown', handleMouseDownOnce);
-    // eslint-disable-next-line no-use-before-define
-    window.addEventListener('keydown', handleFirstTab);
-  }
-
-  function handleFirstTab(e) {
-    if (e.keyCode === 9) {
-      document.body.classList.add(userTabbing);
-      window.removeEventListener('keydown', handleFirstTab);
-      window.addEventListener('mousedown', handleMouseDownOnce);
-    }
-  }
-
-  window.addEventListener('keydown', handleFirstTab);
+  window.removeEventListener("mousedown", handleMouseDownOnce);
+  // eslint-disable-next-line no-use-before-define
+  window.addEventListener("keydown", handleFirstTab);
 }
+
+function handleFirstTab(e) {
+  if (e.keyCode === 9) {
+    document.body.classList.add(userTabbing);
+    window.removeEventListener("keydown", handleFirstTab);
+    window.addEventListener("mousedown", handleMouseDownOnce);
+  }
+}
+
+window.addEventListener("keydown", handleFirstTab);
+
+export default userTabbing;
+
